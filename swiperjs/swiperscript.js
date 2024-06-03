a821eca9c6d7d1bada81bb5d476689268f08df50
Pranav P K <pranavpk1519@gmail.com>
2024-06-03 15:15:49 +0530

    modified js code

--- swiperjs/swiperscript.js
+++ swiperjs/swiperscript.js
@@ -23,36 +23,4 @@ var swiper = new Swiper(".testi-slide-content", {
       slidesPerView: 3,
     }
   }
-
-
-
 });
-
-
-
-let previewContainer = document.querySelector('.review-preview');
-let previewBox = previewContainer.querySelectorAll('.preview');
-
-document.querySelectorAll('.testi-card-wrapper .testi-card').forEach(product => {
-  product.querySelector('.testi-button').onclick = (e) => {
-    previewContainer.style.display = 'flex';
-    let name = product.getAttribute('data-name');
-    previewBox.forEach(preview => {
-      let target = preview.getAttribute('data-target');
-      if (name == target) {
-        preview.classList.add('active');
-      } else {
-        preview.classList.remove('active');
-      }
-    });
-    e.stopPropagation(); // Prevents the event from bubbling up to the product card if necessary
-  }
-})
-
-
-previewBox.forEach(close => {
-  close.querySelector('.fa-times').onclick = () => {
-    close.classList.remove('active')
-    previewContainer.style.display = 'none';
-  }
-})
